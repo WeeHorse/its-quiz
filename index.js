09a1bdc2370bf5452f5ed99fc1397416d620b9a4
Ironboy <ironboy@users.noreply.github.com>
2026-02-05 08:10:20 +0100

    all

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = 5222;
+const app = express();
+
+app.use((_req, res, _next) => {
+  res.sendFile(path.join(__dirname, 'www', 'index.html'));
+});
+
+app.listen(PORT, () => console.log('Listening on http://localhost:' + PORT));
